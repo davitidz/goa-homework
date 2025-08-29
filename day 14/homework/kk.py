65531d0f7ae7fd67ac9f18ceeaff732063854aa8
davitidz <sabsondavitidze@gmail.com>
2025-08-29 23:51:44 +0400

    commit

--- day 14/homework/kk.py
+++ day 14/homework/kk.py
@@ -6,3 +6,30 @@
 
 
 
+num1 = int(input("enter a number: "))
+num2 = int(input("enter a number: "))
+num3 = int(input("enter a number: "))
+
+print(num1 + num2 + num3)
+print(num1 - num2 - num3)
+print(num1 * num2 * num3)
+print(num1 / num2 / num3)
+
+n =5
+print(type(n))
+n = "saba"
+print(type(n))
+n = 1.3
+print(type(n))
+n = "car"
+print(type(n))
+n = 90
+print(type(n))
+n = 2.2
+print(type(n))
+n = "tank"
+print(type(n))
+n = 27
+print(type(n))
+n = 2.3
+print(type(n))
